dbffcee392b5cb26bc1f5d7405dfab50a3f9c451
XMAKE <xmake.studio@gmail.com>
2026-03-20 23:35:07 +0300

    utilizing full frame buffer and config registers

--- README.md
+++ README.md
@@ -2,10 +2,29 @@ One day I ordered random very cheap (7$) 64x64 P3 RGB LED panel. When I recieved
 This driver is most likely can be used to drive similar SUM20XX panels, i've seen that SUM2030 and SUM2032 exists, but of course I haven't tested this.
 
 So what I know now:
-- it supports 10-16 bit color
-- it does that by counting clocks on OE pin (connected to GCLK), so it has to recieve between 2^10=1023 and 2^16=65535 equally spaced pulses per row of display
-- it automagically works with any color depth (from 10 to 16 ig), you just should send more GCLK pulses per row and get more color depth (datasheet says it can recieve up to 30 mHz on GCLK)
-- it has some amount of internal RAM, and you should send next line to his ram, while displaying previous one. 
+- it supports 10-16 bit color per channel (altho I only successfully got 13 bits)
+- it does that by counting clocks on OE (GCLK) pin, and it has internal 8-16x configurable clocks frequency multiplicator, so it has to recieve between 2^10/16=64 and 2^13/8=1024 equally spaced PWM pulses per row of display
+- it also has to recieve 17 addditional GCLK pulses independent of color depth chosen
+- it has 16K bits of internal RAM (2 entire frame buffers), and you can send new frame to RAM, while displaying previous one (double buffering)
 - I think sending new data in can be independent of GCLK, but for simplicity i've made it syncronized (10 bit color perfectly matches 16x4x16=1024 pulses needed to fill next line)
-- when you want to show next line, you should send special command, N=2 (or 3) CLK pulses with high latch. It swaps buffers and resets GCLK counter.
-- it may have other commands. Maybe even up to 32 of them. Altho I haven't found any particulary useful ones.
+- when you want to show next frame, you should send special command, N=2 (or 3) CLK pulses with high latch. It swaps frame buffers and resets GCLK, CLK and LATCH counter.
+- it may have other commands, activated in same way. Altho I haven't found any particulary useful ones.
+
+- it has 3 internal config registers, written with 9, 11 and 13 CLK pulses after pulling LATCH high:
+
+- REG1: 
+Consists of wierd bit number 2, most likely used to achieve more than 13 bits of color depth, but this requires wierd row activation sequence or some other configuration, and 13 bits is plenty for me.
+Also consists of wierd bit number 3, which, when enabled, can do wierd stuff combined with other otherwise seemingly unused bits
+All other bits here and in other registers are most likely those "wierd" bits that only change anything when this bit 3 is enabled.
+
+- REG2:
+6 bits of global brightness (64 levels, but 0 brightness is more like half of max brightness, still useful)
+5 bits of scan select (1-32, my panel is 1/32 scan (64x64), so should be all ones)
+2 bits of color depth select, inverted (00 means 13 bits, 01 means 12 bits, 10 means 11 bits, 11 means 10 bits)
+2 unknown purpose bits (somehow cuts dark colors?)
+1 bit that inverts PWM sequence (useless)
+
+- REG3:
+Bit number 2 is used to enable X16 GCLK multiplication (X8 otherwise)
+Bit number 4, when 0, seems to almost completely disable matrix? this "almost" is wierd tho, and this "almost" can be achieved with bit 3 of REG1 set to 1, so idk
+Otherwise also consists of those "wierd" bits
--- src/main.cpp
+++ src/main.cpp
@@ -17,21 +17,28 @@
 
 #define CLK 7
 #define LAT 17
-#define OE 14 /* also GLCK */
-
-#define COLOR_DEPTH_BITS 10 /* should be between 10 and 16 */
+#define OE  14 /* also GLCK */
 
 #define MATRIX_WIDTH 64 /* can be increased in case of chain link, altho I have only one matrix */
 #define MATRIX_SCAN  32 /* 1/32 scan in my case, also means 64 pixels height */
 
+uint8_t cfg_brightness = 63; // 0-63  (6 bits)
+uint8_t cfg_color_bits = 13; // 10-13 (2 bits)
+
+// in-chip multiplier of GCLK pulses count, as far as I know can be configured to either 8 or 16, 
+// altho I haven't seen any benefit in decreasing it to 8.
+uint8_t cfg_gclk_mul = 16;  
+
+const int storedColorBits = 16; // should always be 16, independent on cfg_color_bits, since we should fill entire RAM of SUM2033
+const int channelsPerChip = 16; // always 16 for SUM2033
+const int chips_count = MATRIX_WIDTH / channelsPerChip;
 
 constexpr uint32_t CLK_MASK = 1UL << CLK;
 constexpr uint32_t GCLK_MASK  = 1UL << OE;
 
 // fast GPIO magik used here, if you want CLK or OE pins to be larger than 31 - change registers or change this magik to gpio_set_level
-#define CLK_PULSE GPIO.out_w1tc = CLK_MASK; GPIO.out_w1ts = CLK_MASK;
-#define GCLK_PULSE  GPIO.out_w1tc = GCLK_MASK;  GPIO.out_w1ts = GCLK_MASK;
-
+#define CLK_PULSE   GPIO.out_w1ts = CLK_MASK;   GPIO.out_w1tc = CLK_MASK;
+#define GCLK_PULSE  GPIO.out_w1ts = GCLK_MASK;  GPIO.out_w1tc = GCLK_MASK;
 
 void selectRow(int row)
 {
@@ -42,16 +49,78 @@ void selectRow(int row)
   gpio_set_level((gpio_num_t)CH_E, (row >> 4) & 1);
 }
 
-void latch(int clk_count)
+void IRAM_ATTR sendPwmClock(int clocks) {
+  while (clocks--) {
+    GCLK_PULSE
+  }
+}
+
+void IRAM_ATTR sendLatch(unsigned char clocks)
 {
   gpio_set_level((gpio_num_t)LAT, HIGH);
 
-  for(int i = 0; i < clk_count; i++)
-  {
+  while(clocks--) {
     CLK_PULSE
   }
 
   gpio_set_level((gpio_num_t)LAT, LOW);
+}
+
+void sendConfiguration(unsigned char latches, unsigned int data) {
+    unsigned char num = chips_count;
+
+    latches = 16 - latches;
+
+    gpio_set_level((gpio_num_t)LAT, 0); 
+
+    while(num--) {
+        for(unsigned char x = 0; x < 16; x++) {
+            unsigned int dataMask = 0x8000 >> x;
+
+            bool en = data & dataMask;
+
+            gpio_set_level((gpio_num_t)R1, en);
+            gpio_set_level((gpio_num_t)G1, en);
+            gpio_set_level((gpio_num_t)B1, en);
+            gpio_set_level((gpio_num_t)R2, en);
+            gpio_set_level((gpio_num_t)G2, en);
+            gpio_set_level((gpio_num_t)B2, en);
+
+            if(num == 0 && x == latches) 
+            { 
+              gpio_set_level((gpio_num_t)LAT, 1); 
+            }
+            CLK_PULSE
+        }
+
+        gpio_set_level((gpio_num_t)LAT, 0); 
+    }
+}
+
+void sendConfigRegs()
+{
+  sendLatch(14); // Pre-active command
+  sendConfiguration(9, 0b0); // Write config register 1
+
+  // 6 bits of brightness
+  uint16_t reg2 = cfg_brightness & 0b111111; 
+
+  // 5 bits after brightness - scan lines register, we have 1/32 scan, so all ones (literally 1984) 
+  reg2 |= (MATRIX_SCAN - 1) << 6; 
+  
+  // another 2 bits is inverted color depth - from 10 to 13 in normal (slow?) mode
+  reg2 |= (~(cfg_color_bits - 10) & 0b11) << 11; 
+  
+  sendLatch(14);  // Pre-active command
+  sendConfiguration(11, reg2); // Write config register 2
+
+  uint16_t reg3 = 0;
+
+  reg3 |= (cfg_gclk_mul >> 4) << 2; // shifting by 4, since 16 = 0b10000 >> 4 = 0b1
+  reg3 |= 1 << 4; // must be 1 for matrix to function correctly
+
+  sendLatch(14); // Pre-active command
+  sendConfiguration(13, reg3); // Write config register 3
 }
 
 void setup() 
@@ -98,69 +167,72 @@ pixel getPixel(uint8_t x, uint8_t y)
 {
   pixel pix;
   
-  pix.r = pix.g = pix.b = ((x == y) * gammaCorrectGrayscale(x * 1023)); // diagonal white line with gradient
-  //pix.r = pix.g = pix.b = gammaCorrectGrayscale(x * 1023);                  // just white gradient
+  // 1040 here everywhere since it is 65535/63
+
+  //pix.r = pix.g = pix.b = ((x == y) * gammaCorrectGrayscale(x * 1040)); // diagonal white line with gradient
+  //pix.r = pix.g = pix.b = y == 1 ? 65535 : ((y == 5) * (x * 1040));     // test
+  //pix.r = pix.g = pix.b = gammaCorrectGrayscale(x * 1040);              // just white gradient
+
+  pix.r = gammaCorrectGrayscale(x * 1040);                  // colorful RG gradient
+  pix.g = gammaCorrectGrayscale(y * 1040);                  // colorful RG gradient
   
   return pix;
 }
 
 void loop() 
 {
-  const int colorBits = 16; // should always be 16, independent on COLOR_DEPTH_BITS, since we should fill entire RAM of SUM2033
-  const int channelsPerChip = 16;
-  const int chips = MATRIX_WIDTH / channelsPerChip;
-  const int rowsPerChip = MATRIX_SCAN;
+  //cfg_brightness = (millis() / 100) % 64; // to test brightness
+  //cfg_color_bits = 10 + (millis() / 1000) % 4; // to test color depth
 
-  const int CLK_pulses_per_row = chips * channelsPerChip * colorBits;
+  sendConfigRegs();
 
-  const int additionalColorBits = COLOR_DEPTH_BITS - log2(CLK_pulses_per_row);
-  const int glck_pulses = pow(2, additionalColorBits);
+  static int c = -1;
+  c++;
+  // updating entire frame in first 2 loops (since double buffering)
+  bool fullUpdate = c < 2;
 
-  uint64_t test = millis() / 200;
+  int gclk_pulses_per_line = pow(2, cfg_color_bits) / cfg_gclk_mul;
 
-  for(uint8_t row = 0; row < rowsPerChip; row++) 
+  sendLatch(3); // vsync + buffer swap
+
+  for(uint8_t line = 0; line < MATRIX_SCAN; line++) 
   {
+    sendPwmClock(17); // should be here, and 17 clocks exactly
+    selectRow(line);
+    sendPwmClock(gclk_pulses_per_line);
+
+    if(!fullUpdate)
+      continue;
+    
     for(uint8_t channel = 0; channel < channelsPerChip; channel++) 
     {
-      for(uint8_t chip = 0; chip < chips; chip++)
+      for(uint8_t chip = 0; chip < chips_count; chip++)
       {
         const auto x = channel + channelsPerChip * chip;
-        const auto p1 = getPixel(x, row);
-        const auto p2 = getPixel(x, row + rowsPerChip);
+        const auto p1 = getPixel(x, line);
+        const auto p2 = getPixel(x, line + MATRIX_SCAN);
 
-        for(int8_t bit = colorBits - 1; bit >= 0; bit--) // inverted bit order
+        for(int8_t bit = storedColorBits - 1; bit >= 0; bit--) // inverted bit order
         {
           const uint16_t m = 1UL << bit;
-          
-          // full color write - kinda slow this way. Uncomment 6 lines below and comment test pattern lines to see full color tests
-          //gpio_set_level((gpio_num_t)R1, p1.r & m);
-          //gpio_set_level((gpio_num_t)G1, p1.g & m);
-          //gpio_set_level((gpio_num_t)B1, p1.b & m);
-          //gpio_set_level((gpio_num_t)R2, p2.r & m);
-          //gpio_set_level((gpio_num_t)G2, p2.g & m);
-          //gpio_set_level((gpio_num_t)B2, p2.b & m);
+        
+          gpio_set_level((gpio_num_t)R1, p1.r & m);
+          gpio_set_level((gpio_num_t)G1, p1.g & m);
+          gpio_set_level((gpio_num_t)B1, p1.b & m);
+          gpio_set_level((gpio_num_t)R2, p2.r & m);
+          gpio_set_level((gpio_num_t)G2, p2.g & m);
+          gpio_set_level((gpio_num_t)B2, p2.b & m);
 
-          // alternative - test patterns:
-          // (it is normal that in test pattern mode only ony half of matrix works, it is 2X speed this way)
-          gpio_set_level((gpio_num_t)R1, x == ((test % MATRIX_WIDTH)));  // test pattern for checking individual channels (ref vertical lines)
-          gpio_set_level((gpio_num_t)G1, bit == (test % colorBits));     // test pattern to check individual color bits (in green)
-          gpio_set_level((gpio_num_t)B1, row == (test % rowsPerChip));   // test pattern for blue horizontal lines
+          if(chip == chips_count - 1 && bit == 0)
+          {
+            // send a latch for 1 clock after every chip was written to 
+            gpio_set_level((gpio_num_t)LAT, 1); 
+          }
 
           CLK_PULSE
-
-          for(uint8_t i = 0; i < glck_pulses; i++)
-          {
-            GCLK_PULSE
-          }
         }
       }
-      latch(0);
+      gpio_set_level((gpio_num_t)LAT, 0);
     }
-
-    latch(2); // 2, 3 works?
-    GCLK_PULSE // to prevent ghosting
-    selectRow(row);
   }
 }
-
-
